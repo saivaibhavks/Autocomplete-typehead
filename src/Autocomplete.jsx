e154eaf2cc7682f3dbc445b7aebf552c941dbbf1
Saivaibhav K S (OSV) <saivaibhav.s.osv@fedex.com>
2024-11-17 20:21:29 +0530

    changed

--- src/Autocomplete.jsx
+++ src/Autocomplete.jsx
@@ -5,6 +5,7 @@ const Autocomplete = () => {
   const fetchProducts = async (e) => {
     console.log("val", e.target.value);
     const json = await fetch("https://dummyjson.com/products/search?q=phone");
+    const reps = json.json();
     console.log("json", json);
   };
   return (
