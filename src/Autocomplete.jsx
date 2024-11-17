ee9a6b6de76e9f7861b32241dd4525c4e8bda33e
Saivaibhav K S (OSV) <saivaibhav.s.osv@fedex.com>
2024-11-17 19:59:14 +0530

    added input field

--- src/Autocomplete.jsx
+++ src/Autocomplete.jsx
@@ -1,7 +1,10 @@
+import { useState } from "react";
+
 const Autocomplete = () => {
+  cosnt[(inputVal, setInputVal)] = useState("");
   return (
     <div className="container">
-      <input type="text" />
+      <input type="text" placeholder="Enter input" />
     </div>
   );
 };
